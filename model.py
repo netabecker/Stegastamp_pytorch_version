e8dd4f38220638d14f0de81a5aa84609630b6e54
netabecker <netabec3@gmail.com>
2023-01-09 10:23:58 +0200

    push before pulling previous net

--- model.py
+++ model.py
@@ -294,11 +294,11 @@ class StegaStampDecoder_Unet(nn.Module):
         x1 = self.inc(input)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
-        # x4 = self.down3(x3)
+        x4 = self.down3(x3)
         # x5 = self.down4(x4)
         # x = self.up1(x5, x4)
-        # x = self.up2(x, x3)
-        x = x3
+        x = x4
+        x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
         logits = self.outc(x)
